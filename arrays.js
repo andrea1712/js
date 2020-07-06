b233d7691daf53af24843d11e888a0605aa1be5d
Andrea Sanchez <jennyae82@gmail.com>
2020-07-05 22:24:39 -0500

    Este es la versión 1

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var arrayUno = [ 
+    ['A','J','A','H','A'],
+    ['B','H','R','J','A'],
+    ['A','C','O','S','O'],
+    ['S','L','M','G','V'],
+    ['A','Z','A','E','O'],
+];
+console.log (arrayUno);
+console.log(arrayUno[0][0],arrayUno[1][1],arrayUno[2][2],arrayUno[3][3],arrayUno[4][4]);
+console.log(arrayUno[4][0],arrayUno[3][1],arrayUno[2][2],arrayUno[1][3],arrayUno[4][0]);
+console.log(arrayUno[0][2],arrayUno[1][2],arrayUno[2][2],arrayUno[3][2],arrayUno[4][2]);
+console.log(arrayUno[2][0],arrayUno[2][1],arrayUno[2][2],arrayUno[2][3],arrayUno[2][4]);
